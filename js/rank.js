0b183e7491be42e1d85a04521823df07b59be505
YouJeong Lee <ijyk3061@gmail.com>
2019-05-18 02:50:36 +0900

    Rebuild ResponsiveWeb

--- js/rank.js
+++ js/rank.js
@@ -26,7 +26,7 @@ function userSessionCheck_rank() {
                 setTimeout(function() {
                     var ranking_list = "";
                     for(var i = sort_name.length - 1, j=1; i >= 0; i--, j++) {
-                        ranking_list += "<tr><th scope='row'>" + j + "</th><td><img class='rank-img' src='img/item" + sort_img[i] + ".png'>"+ "  " + sort_name[i] +"</td><td>" + sort_exp[i] + "</td></tr>"
+                        ranking_list += "<tr><th scope='row'>" + j + "</th><td><img class='rank-img' src='img/item" + sort_img[i] + ".png'>" + "</td><td>" + sort_name[i] +"</td><td>" + sort_exp[i] + "</td></tr>"
                     }
 
                     document.getElementById("rankTable").innerHTML = ranking_list;
